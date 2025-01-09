445d6f321ffc2196f334890346a1499f0d50b59f
Tom <tbrigham@hotmail.com>
2025-01-08 20:56:10 -0700

    Init commit

---
+++ credentials.auto.tfvars
@@ -0,0 +1,3 @@
+proxmox_api_url = "https://192.168.2.151:8006/api2/json"
+proxmox_api_token_id = "tom@pam!terraform"
+proxmox_api_token = "b007f200-d424-4b2a-a90e-fe56142d5be7"
---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc6"
+    }
+  }
+}
+
+provider "proxmox" {
+  # Configuration options
+}
