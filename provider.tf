01b0ae86b1ae74fbca71b9f1e15c60ed4689e701
Tom <tbrigham@hotmail.com>
2025-01-08 21:16:14 -0700

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,22 @@ terraform {
   }
 }
 
+variable proxmox_api_url {
+  type        = string
+}
+
+variable proxmox_api_token_id {
+  type        = string
+}
+
+variable proxmox_api_token {
+  type        = string
+}
+
 provider "proxmox" {
   # Configuration options
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token
+  pm_tls_insecure = true
 }
